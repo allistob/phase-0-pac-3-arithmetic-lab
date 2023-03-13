90aaa478109a7dd9ad908412dc9b5d2cba32fa5a
Allison Tobar <allisontobarrodriguez@gmail.com>
2023-03-12 23:47:11 -0400

    calculator challenge

---
+++ index.js
@@ -0,0 +1,51 @@
+function add() {
+    
+  }
+
+ function subtract(){
+    
+  }
+
+
+  function multiply(){
+    
+  }
+
+  function divide(){
+    
+  }
+
+  function add(a,b) {
+    return a + b;
+  }
+
+function subtract(a, b) {
+    return a - b;
+}
+
+function multiply(a, b) {
+ return a * b;   
+}
+
+function divide(a, b){
+    return a / b;
+
+    
+}
+
+function increment(a){
+    let number = a + 1;
+    return number;
+}
+
+function decrement(a){
+   let number = a - 1;
+    return number;
+}
+
+function makeInt(string){
+       return parseInt(string,10)
+}
+function preserveDecimal(string){
+       return parseFloat(string,10)
+    }
